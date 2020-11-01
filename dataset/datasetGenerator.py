6a0961646fe7360d7283e4b5a938575b380ab272
JPCatarino <jorge.catarino@ua.pt>
2020-11-01 17:23:36 +0000

    [DATA] Add add_id to datasetGenerator

--- dataset/datasetGenerator.py
+++ dataset/datasetGenerator.py
@@ -18,11 +18,48 @@ def build_league(file):
     return ret
 
 
+def checkIfAlternate(val, alt_list):
+    for n in alt_list:
+        if val == n:
+            return True
+    return False
+
+
+def add_id(players, teams):
+    teams_tree = etree.parse(teams)
+    teams_root = teams_tree.getroot()
+    players_tree = etree.parse(players)
+    players_root = players_tree.getroot()
+
+    player_country_root = etree.Element('players')
+    pc_tree = etree.ElementTree(player_country_root)
+
+    alt = []
+
+    for team in teams_root:
+        team_str = team.find('strTeam').text
+        team_id_elem = team.find('idTeam')
+        team_alt_str = team.find("strAlternate").text
+
+        if team_alt_str is not None:
+            alt = team_alt_str.split(', ')
+
+        for player in players_root:
+            player_club_str = player.find('Club').text
+
+            if player_club_str == team_str or checkIfAlternate(player_club_str, alt):
+                player.append(team_id_elem)
+                player_country_root.append(player)
+
+    pc_tree.write('players\\'+teams.split('_')[0].split('\\')[1] + '_playersWithID.xml', xml_declaration=True, encoding='utf-8', pretty_print=True)
+
+
 if __name__ == '__main__':
     datasetRoot = etree.Element('leagues')
     dtTree = etree.ElementTree(datasetRoot)
 
     for filename in os.listdir('teams'):
         datasetRoot.append(build_league('teams\\' + filename))
+        add_id('players\\players_fifa21.xml', 'teams\\' + filename)
 
-    dtTree.write('leagueTeams.xml', xml_declaration=True, encoding='utf-8', pretty_print=True)
+    dtTree.write('teams\\leagueTeams.xml', xml_declaration=True, encoding='utf-8', pretty_print=True)
