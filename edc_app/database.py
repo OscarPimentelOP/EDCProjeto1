e4d156519920cbee158df6b42a27ce081e7f733e
JPCatarino <jorge.catarino@ua.pt>
2020-11-05 19:53:43 +0000

    [DB] remove RunSimpleQuery, change RunQuery to have command empty by default

--- edc_app/database.py
+++ edc_app/database.py
@@ -10,26 +10,25 @@ def connect_db():
     global session
     session = BaseXClient.Session('localhost', 1984, 'admin', 'admin')
 
+
 def disconnect_db():
     global session
     if session:
         session.close()
 
-def runQuery(file, command):
+
+def runQuery(file, command=''):
     connect_db()
-    res = session.execute('xquery \n' + open(os.path.join(STATICFILES_DIRS[0],"queries", file),'r').read() + '\n' + command)
+    res = session.execute(
+        'xquery \n' + open(os.path.join(STATICFILES_DIRS[0], "queries", file), 'r').read() + '\n' + command)
     disconnect_db()
     return res
 
-def runSimpleQuery(file):
-    connect_db()
-    res = session.execute('xquery \n' + open(os.path.join(STATICFILES_DIRS[0],"queries", file),'r').read())
-    disconnect_db()
-    return res
 
 def list_all_players():
-    res = runSimpleQuery("ListAllPlayers.xq")
+    res = runQuery("ListAllPlayers.xq")
     return res
+
 
 def order_players_pos(pos):
     res = runQuery('OrderPlayersOverallArgument.xq', 'local:playersByPosition({})'.format(pos))
@@ -37,9 +36,9 @@ def order_players_pos(pos):
 
 
 def main():
-    #print(list_all_players())
-    print(order_players_pos('"ST"'))
+    print(list_all_players())
+    #print(order_players_pos('"ST"'))
+
 
 if __name__ == "__main__":
     main()
-
